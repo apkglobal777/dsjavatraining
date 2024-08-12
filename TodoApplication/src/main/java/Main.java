849ef17102a8d0477de6027bc28b3a3a4f8d63a2
simrasarfaraz <simra.sarfaraaz@gmail.com>
2024-08-12 15:39:12 +0530

    Add update delete features in todoapp

--- TodoApplication/src/main/java/Main.java
+++ TodoApplication/src/main/java/Main.java
@@ -7,6 +7,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Random;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -15,7 +16,7 @@ public class Main {
     //create Lables and TextFields for todoApp
     static JLabel taskNamelb, taskDatelb, tasktimelb;
     static JTextField taskNametf, taskDatetf, tasktimetf;
-    static JButton addTaskbt;
+    static JButton addTaskbt, deleteTaskbt, updateTaskbt, viewTaskbt;
     public static void main(String[] args) {
         //Add maven in your project
         //Create client and establish the mongo database connection
@@ -40,6 +41,9 @@ public class Main {
         tasktimetf = new JTextField();
 
         addTaskbt = new JButton("Add Task");
+        deleteTaskbt = new JButton("Delete Task");
+        updateTaskbt = new JButton("Update Task");
+        viewTaskbt = new JButton("View Task");
 
         //to set the size and position of UI components
         taskNamelb.setBounds(50, 50, 150, 40);
@@ -49,8 +53,10 @@ public class Main {
         taskNametf.setBounds(180, 50, 120, 30);
         taskDatetf.setBounds(180, 80, 120, 30);
         tasktimetf.setBounds(180, 110, 120, 30);
-        addTaskbt.setBounds(50, 150, 100, 30);
-
+        addTaskbt.setBounds(10, 150, 100, 30);
+        deleteTaskbt.setBounds(110, 150, 100, 30);
+        updateTaskbt.setBounds(210, 150, 100, 30);
+        viewTaskbt.setBounds(310, 150, 100, 30);
         //to attach the UI components into frame
         frame.add(taskNamelb);
         frame.add(taskDatelb);
@@ -59,9 +65,11 @@ public class Main {
         frame.add(taskDatetf);
         frame.add(tasktimetf);
         frame.add(addTaskbt);
-
+        frame.add(deleteTaskbt);
+        frame.add(viewTaskbt);
+        frame.add(updateTaskbt);
         //to set the size of frame
-        frame.setSize(400, 300);
+        frame.setSize(450, 300);
         frame.setLayout(null);
         frame.setResizable(false);
         frame.getContentPane().setBackground(Color.GRAY);
@@ -76,23 +84,66 @@ public class Main {
             }
         });
 
+        //to update the task
+        updateTaskbt.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //update task
+                updateTask();
+            }
+        });
+
+        //to delete the task
+        deleteTaskbt.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //delete the task
+                deleteTask();
+            }
+        });
+
     }
+
+    private static void deleteTask() {
+        //to access the collection
+        var todoCollection = todoDB.getCollection("todoList");
+        String deletetaskId = JOptionPane.showInputDialog(null,
+                "Enter task id to delete the task");
+        todoCollection.deleteOne(new Document("taskId", Integer.parseInt(deletetaskId)));
+    }
+
+    private static void updateTask() {
+        //to access the collection
+        var todoCollection = todoDB.getCollection("todoList");
+        //to update the task status
+
+        String taskId = JOptionPane.showInputDialog(null, "Enter task id to update status");
+        todoCollection.updateOne(new Document("taskId", Integer.parseInt(taskId)),
+                    new Document("$set", new Document("taskStatus", true)));
+
+
+    }
+
     private static void insertTaskIntoDB() {
         //to access the collection
         var todoCollection = todoDB.getCollection("todoList");
         //to create the document and add in collection
+        var random = new Random();
         var document = new Document();
         //add data into document
         document.append("taskName", taskNametf.getText());
         document.append("taskDate", taskDatetf.getText());
         document.append("taskTime", tasktimetf.getText());
         document.append("taskStatus", false);
+        document.append("taskId", random.nextInt(999));
         //to add the document into collection
         todoCollection.insertOne(document);
         JOptionPane.showMessageDialog(null, "Task Added Successfully");
         //clear the form
         clearForm();
     }
+
+
 
     private static void clearForm() {
         taskDatetf.setText("");
